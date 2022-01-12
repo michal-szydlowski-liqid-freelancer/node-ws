311fcf754eddaed60a69e074e242f4fa37fb6145
michal-szydlowski-liqid-freelancer <michal.szydlowski-freelancer@liqid.de>
2022-01-12 13:39:45 +0100

    Potato

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ var device = require('./routes/device');
 
 var app = express();
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Methods'
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Methods'
+//   );
+//   next();
+// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
